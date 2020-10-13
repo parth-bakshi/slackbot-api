16ad56966c3303679701269b89af1f8a4968d850
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-10-13 14:54:58 +0530

    [token] credentials updated

---
+++ keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    secretEncryptionKey: "tobeset",
+    slackClientID:"",
+    slackClientSecretKey:"",
+    slackBotToken:"xoxb-1432948572801-1406470158487-KXvx3NAiKcXSPrFCf3IIHLfH",
+    redirectedURL:"",
+    mongoPassword:"qwertyuioP1!"
+}
