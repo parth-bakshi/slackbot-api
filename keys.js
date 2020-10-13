af4d5fcaa756f9f8f218af62cee0cb8fac0d8b81
parth-bakshi <parth.bakshi@mckinleyrice.com>
2020-10-13 15:03:09 +0530

    [token] token updated to heroku env var

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ module.exports = {
     secretEncryptionKey: "tobeset",
     slackClientID:"",
     slackClientSecretKey:"",
-    slackBotToken:"xoxb-1432948572801-1406470158487-KXvx3NAiKcXSPrFCf3IIHLfH",
+    slackBotToken: process.env.slackBotToken,
     redirectedURL:"",
     mongoPassword:"qwertyuioP1!"
 }
